ac0ad48306defd027c237a896ec9c5b7bab8c2ba
Atharva Sabde <atharvaa.sabde@gmail.com>
2021-06-07 19:18:07 +0530

    final details

--- Home_Services/app/src/main/java/com/project/home_services/Signup.java
+++ Home_Services/app/src/main/java/com/project/home_services/Signup.java
@@ -15,11 +15,11 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //----disable titlebar code
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
-        getSupportActionBar().hide(); //hide the title bar
-
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+//        getSupportActionBar().hide(); //hide the title bar
+//
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
 
 
         setContentView(R.layout.activity_signup);
