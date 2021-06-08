f88a7179846060fbbe1313260cb1a722b2e5c6f0
Atharva Sabde <atharvaa.sabde@gmail.com>
2021-06-08 20:58:30 +0530

    added progressbar

--- Home_Services/app/src/main/java/com/project/home_services/Verification.java
+++ Home_Services/app/src/main/java/com/project/home_services/Verification.java
@@ -6,14 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.chaos.view.PinView;
 
 public class Verification extends AppCompatActivity implements View.OnClickListener {
 
     private Button  verifyreqq;
     private Button verifyy;
 
+    private TextView TXTVnumber;
+    private PinView OTP;
+
+    private ProgressBar progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +34,63 @@ public class Verification extends AppCompatActivity implements View.OnClickListe
 
         verifyy = findViewById(R.id.BTNverify);
         verifyy.setOnClickListener(this);
+
+        TXTVnumber = findViewById(R.id.TXTVnumber);
+        OTP = findViewById(R.id.OTP);
+
+        progressbar = findViewById(R.id.VerifyProgressbar);
+    }
+
+
+    private boolean validateNumber(){
+        String number = TXTVnumber.getEditableText().toString().trim();
+        if(number.isEmpty()){
+            TXTVnumber.setError("Field can't be Empty!");
+            return true;
+        }else if(number.length()!=10){
+            TXTVnumber.setError("Enter Valid Number!");
+            return true;
+        }else{
+            TXTVnumber.setError(null);
+            return false;
+        }
+    }
+
+    private boolean validateOTP(){
+        String number = OTP.getEditableText().toString().trim();
+        if(number.isEmpty()){
+            OTP.setError("Field can't be Empty!");
+            return true;
+        }else if(number.length()!=10){
+            OTP.setError("Enter Valid Number!");
+            return true;
+        }else{
+            OTP.setError(null);
+            return false;
+        }
     }
 
     public void onClick(View v){
-        switch (v.getId()){
-            case R.id.BTNverifyreq:
-                Toast.makeText(this, "Sending OTP...", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.BTNverify:
-                Toast.makeText(this, "Verifying...", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(this, Details.class));
-
-
-                break;
-
+        if(validateNumber() | validateOTP()){
+            return;
         }
+        else{
+            switch (v.getId()){
+                case R.id.BTNverifyreq:
+                    Toast.makeText(this, "Sending OTP...", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.BTNverify:
+                    if(!validateOTP()){
+                        Toast.makeText(this, "Verifying...", Toast.LENGTH_SHORT).show();
+                        progressbar.setVisibility(View.VISIBLE);
+                        startActivity(new Intent(this, Details.class));
+                    }
+                    break;
+
+            }
+        }
+
 
 //        Intent Details = new Intent(this, Details.class);
 //
